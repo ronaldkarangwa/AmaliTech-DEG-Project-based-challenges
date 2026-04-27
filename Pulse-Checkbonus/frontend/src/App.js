e9aa43ff57c3a8c68c60153f7936ce39d8ee03d2
Ronald <gahimaronald1998@gmail.com>
2026-04-27 22:21:48 +0200

    fixed wrong endpoint

--- Pulse-Checkbonus/frontend/src/App.js
+++ Pulse-Checkbonus/frontend/src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API = "http://localhost:5000";
+const API = "http://127.0.0.1:5000";
 
 function App() {
   const [monitors, setMonitors] = useState([]);
